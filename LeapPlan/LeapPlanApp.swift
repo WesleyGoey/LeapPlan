23569adc71cd71a7ba7fe11a120ed3901ba3f286
WesleyGoey <wesley.goey@gmail.com>
2026-05-28 08:38:35 +0700

    basic

---
+++ LeapPlan/LeapPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeapPlanApp.swift
+//  LeapPlan
+//
+//  Created by Wesley Goey on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LeapPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
